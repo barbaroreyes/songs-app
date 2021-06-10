496f9428f22c5f3a1ec4b09629dde51e3cef80fd
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-06-10 09:44:43 -0500

    update-song-color

--- src/compononet/allsongs.js
+++ src/compononet/allsongs.js
@@ -4,9 +4,9 @@ const Display = ({allSongs,selectFw,handleDeleteW,history}) => {
  console.log('allSongs',allSongs)
   const loaded = () =>{
     return(
-        <div className="songs">
+        <div className="songs  ">
           {allSongs.map((item,i)=>{
-            return (<div className='single' key={i}>
+            return (<div className='single grow bg-green' key={i}>
               <h3>{item.name}</h3>
                  <p>{item.time}</p>
                  <p>{item.author}</p>
